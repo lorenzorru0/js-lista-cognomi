fc29682ea2a987991c493bc885c85ec591a51041
lorenzorru0 <lorenzoorru999@gmail.com>
2021-08-27 10:22:15 +0200

    Add some style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function aggiungiCognome() {
     var cognomeAggiunto = document.getElementById("cognomeAggiunto").value;
-    cognomeAggiunto[0].toUpperCase();
+    cognomeAggiunto = cognomeAggiunto[0].toUpperCase() + cognomeAggiunto.slice(1).toLowerCase();
 
     var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
     cognomi.push(cognomeAggiunto);
@@ -11,7 +11,10 @@ function aggiungiCognome() {
 
     document.getElementById("arrayCognome").innerHTML = 'L array è composto da: ';
     for (var i = 0; i < cognomi.length; i++) {
-        document.getElementById("arrayCognome").innerHTML += cognomi[i] + ',  ';
+        var ul = document.getElementById("cognomiList");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(cognomi[i]));
+        ul.appendChild(li);
     }
     document.getElementById("posizioneCognome").innerHTML += 'Il cognome aggiunto è posizionato in ' + index + 'a posizione.';
 }
